f44f491db022170581a10765275c8dcdf38f241e
Angélica María D <angelidu@gmail.com>
2017-02-21 18:37:54 -0500

    reading a file

---
+++ student.rb
@@ -0,0 +1,7 @@
+# Lee el archivo students.txt e imprime su contenido
+
+txt_file = open("studien.txt")# Open file
+
+contenido = txt_file.read # Read file content and save in content variable
+
+puts contenido # Puts content twice
