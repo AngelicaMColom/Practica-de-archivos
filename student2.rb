02ce39a5c56fbb9906ba28e1977715d8e39cc942
Angélica María D <angelidu@gmail.com>
2017-02-21 18:59:02 -0500

    second exercise

--- student2.rb
+++ student2.rb
@@ -1,1 +1,10 @@
 # Itera sobre cada una de las lineas del archivo students.txt e imprime cada linea de la siguiente forma:
+
+txt_file = open("studien.txt")# Open file
+
+
+txt_file.each_line do |m|
+  print "Maker: #{m}"
+end
+
+txt_file.close #close the file
