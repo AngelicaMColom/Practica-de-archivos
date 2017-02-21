1eaefd6214ac1b94455666aebef08961bdd12b58
Angélica María D <angelidu@gmail.com>
2017-02-21 18:43:44 -0500

    modification

--- student.rb
+++ student.rb
@@ -5,3 +5,5 @@ txt_file = open("studien.txt")# Open file
 contenido = txt_file.read # Read file content and save in content variable
 
 puts contenido # Puts content twice
+
+txt_file.close #close the file
---
+++ student2.rb
@@ -0,0 +1,1 @@
+# Itera sobre cada una de las lineas del archivo students.txt e imprime cada linea de la siguiente forma:
